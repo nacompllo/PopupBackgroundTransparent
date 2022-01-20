a72e365ef6b055be9fc8a9a844c19c6ae4dd6dd1
Sergio Lillo <sergio.lillo@rethinkmarketing.es>
2022-01-20 17:38:50 +0100

    Add project files.

---
+++ PopupBackgroundTransparent/PopupBackgroundTransparent/SamplePopup.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopupBackgroundTransparent
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SamplePopup : Popup
+    {
+        public SamplePopup()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Dismiss(null);
+        }
+    }
+}
